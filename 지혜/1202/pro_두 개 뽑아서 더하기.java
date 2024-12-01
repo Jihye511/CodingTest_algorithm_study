4621765fde39c1c9371aa207081d08e20e0f03c1
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-12-02 08:17:05 +0900

    프로그래머스 깊

---
+++ 지혜/1202/pro_두 개 뽑아서 더하기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        TreeSet<Integer> list = new TreeSet<>();
+        for(int i =0; i<numbers.length-1; i++){
+            for(int j =i+1; j<numbers.length; j++){
+                int hap = numbers[i]+numbers[j];
+                list.add(hap);
+            }
+        }
+        answer = new int[list.size()];
+        int index = 0;
+        for (int num : list) {
+            answer[index++] = num;
+        }
+        return answer;
+    }
+}
