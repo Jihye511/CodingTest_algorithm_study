0c68e1ce048b3e12a81d1c52cd1567bb6b0a5714
seoyeon2001 <syeon1250@naver.com>
2025-01-03 23:33:25 +0900

    백준 자료구조

---
+++ 서연/0103/22942.py
@@ -0,0 +1,27 @@
+import sys
+
+N = int(sys.stdin.readline())
+circles = []
+
+for i in range(N):
+    x, r = map(int, sys.stdin.readline().split())
+    circles.append((x-r, i)) # 왼
+    circles.append((x+r, i)) # 오
+
+circles.sort()
+# print(circles)
+
+stk = []
+for c in circles:
+    if stk:
+        if stk[-1][1] == c[1]:
+            stk.pop()
+        else:
+            stk.append(c)
+    else:
+        stk.append(c) 
+    
+if stk:
+    print('NO')
+else:
+    print('YES')
