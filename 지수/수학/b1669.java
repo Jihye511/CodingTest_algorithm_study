68d543fc816af3cb82cd64c38b4a7ef32f13979e
jisoo <wlttnrla@gmail.com>
2025-01-10 08:58:44 +0900

    백준 수학

---
+++ 지수/수학/b1669.java
@@ -0,0 +1,34 @@
+package 수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b1669 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int monkey = Integer.parseInt(st.nextToken());
+        int dog = Integer.parseInt(st.nextToken());
+
+        if (monkey == dog) {
+            System.out.println(0);
+            return;
+        }
+
+        double interval = dog - monkey;
+
+        double a = Math.sqrt(interval);
+
+        if (a - (int) a > 0) {
+            if (interval - Math.pow((int) a, 2) <= (int) a) {
+                System.out.println(2 * (int) a - 1 + 1);
+            } else {
+                System.out.println(2 * (int) a - 1 + 2);
+            }
+        } else {
+            System.out.println(2 * (int) a - 1);
+        }
+    }
+}
