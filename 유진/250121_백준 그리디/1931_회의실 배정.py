814eab21fcf856f1e7b0e80cfd308077ab9444a3
g0yujin <dbwbsqhd016@gmail.com>
2025-01-21 23:32:15 +0900

    백준 그리디

---
+++ 유진/250121_백준 그리디/1931_회의실 배정.py
@@ -0,0 +1,20 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+end = 0
+answer = 0
+
+arr = []
+
+for _ in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    arr.append([a, b])
+
+arr.sort(key=lambda x: (x[1], x[0]))
+
+for newStart, newEnd in arr:
+    if end <= newStart:
+        answer += 1
+        endPoint = newEnd
+print(answer)
