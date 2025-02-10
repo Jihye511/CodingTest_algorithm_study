835c1ad8e14173742885472fd48c79192737b4e4
JiELee1 <dlwldmscjstk@naver.com>
2025-02-10 23:50:23 +0900

    백준 백트래킹

---
+++ 지은/2025년2월/0210/1182.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static int N, S, result;
+    static int[] subsequence;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        subsequence = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            subsequence[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, 0);
+
+
+        if (S == 0) {
+            result--;
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dfs(int depth, int sum) {
+        if (depth == N) {
+            if (sum == S) {
+                result++;
+            }
+            return;
+        }
+        dfs(depth + 1, sum + subsequence[depth]);
+        dfs(depth + 1, sum);
+    }
+}
---
+++ 지은/2025년2월/0210/15666.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static int N, M;
+    static int[] arr, printArr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        printArr = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int start, int depth) {
+        if(depth == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(printArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = -1;
+        for(int i=start; i<N; i++) {
+            int now = arr[i];
+            if(before != now) {
+                before = now;
+                printArr[depth] = arr[i];
+                dfs(i, depth + 1);
+            }
+
+        }
+    }
+}
