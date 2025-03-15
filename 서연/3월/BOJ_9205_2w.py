df023052fe85862369a1ebe393b79f89937b72fb
seoyeon2001 <syeon1250@naver.com>
2025-03-16 00:22:15 +0900

    백준 골드 5 9205 코멘트 추가

--- 서연/3월/BOJ_9205_2w.py
+++ 서연/3월/BOJ_9205_2w.py
@@ -3,20 +3,24 @@ from collections import deque
 
 def bfs():
     q = deque([(h_x, h_y)]) #시작점을 큐에 담아준다
-    print(q)
 
     while q:
+        # 1)
         x, y = q.popleft()
         if abs(x - f_x) + abs(y - f_y) <= 1000:
             print('happy')
             return
         
+        # 2)        
         for i in range(n):
             if not visited[i]: # 방문하지 않은 편의점 확인
+                # 2-1)
                 new_x, new_y = stores[i]
                 if abs(x - new_x) + abs(y - new_y) <= 1000:
                     visited[i] = True
                     q.append((new_x, new_y))
+    
+    # 2-2)
     print('sad')
     return
 
@@ -38,7 +42,22 @@ for _ in range(t):
     # 페스티벌 좌표
     f_x, f_y = map(int,sys.stdin.readline().split())
     
-    visited = [False for _ in range(n+1)]
+    visited = [False for _ in range(n)] # 편의점 방문 확인
     
     # 실행
     bfs()
+
+
+'''
+풀이
+- 한 번에 이동할 수 있는 최대 거리는 1000m
+- 편의점에서 최대 구매할 수 있는 맥주는 20개
+
+- 처음에는 편의점을 다 들려야한다고 생각했는데 그럴 필요가 없었음!
+-- 그래서 bfs로 확인하는 것이 더 좋음
+
+1. 지금 위치에서 페스티벌 장소까지 거리가 1000m 이내이면 무조건 happy
+2. 그렇지 않다면 갈 수 있는 편의점(1000m이내 편의점)이 있는지 확인해야 함
+2-1. 있다면 그 편의점으로 이동하고 큐에 넣어줌
+2-2. 없다면 sad
+'''
